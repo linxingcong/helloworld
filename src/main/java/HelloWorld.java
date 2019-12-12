ad002439cfe0e46f4002ae1159b0c74b7191cf53
lxc <lxc@qq.com>
2019-12-12 23:03:29 +0800

    lin2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -14,6 +14,9 @@ public class HelloWorld {
         System.out.println("4444");
         System.out.println("5555");
         System.out.println("6666");
+
+
+        System.out.println("lin");
     }
 
 }
