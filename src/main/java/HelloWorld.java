f8211c4fc6da02e243180d0a9b0b97c413382241
lxc <lxc@qq.com>
2020-02-14 23:51:33 +0800

    xing1206

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("xing1");
         System.out.println("xing2");
         System.out.println("xing4");
+        System.out.println("xing1206");
     }
 
 }
