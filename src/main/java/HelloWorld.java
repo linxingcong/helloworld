f999e1f0a359f139eafefeff9ac69cf69c14f41d
lxc <lxc@qq.com>
2020-02-15 17:48:45 +0800

    xing3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("xing1");
         System.out.println("xing2");
+        System.out.println("xing3");
 
     }
 
