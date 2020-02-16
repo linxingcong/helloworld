b499d9bfde2b536d7ef6153de1c8cc9c115bec96
lxc <lxc@qq.com>
2020-02-16 10:52:56 +0800

    test1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,16 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("xing1");
-        System.out.println("xing2");
-        System.out.println("xing3");
-        System.out.println("xing4");
-        System.out.println("xing5");
-        System.out.println("test7");
-        System.out.println("test8");
-        System.out.println("dev6");
-        System.out.println("dev7");
-        System.out.println("dev8");
+        System.out.println("test1");
+
 
     }
 
