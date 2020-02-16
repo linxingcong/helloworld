6a764a8137828a185fca5d6790bdc425b13094c3
lxc <lxc@qq.com>
2020-02-16 11:00:54 +0800

    dev1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("xing1");
-        System.out.println("xing2");
-        System.out.println("xing3");
-        System.out.println("xing4");
-        System.out.println("xing5");
-        System.out.println("dev6");
-        System.out.println("dev7");
-        System.out.println("dev8");
+        System.out.println("dev1");
 
     }
 
